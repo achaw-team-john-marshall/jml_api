c171262fc30d464b455f8a402ec893e5044ecaca
fathergoose <al@ilseman.com>
2016-02-06 21:04:53 -0600

    installed and generated angular application

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jmlApiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jmlApiApp
+ */
+angular.module('jmlApiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jmlApiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jmlApiApp
+ */
+angular.module('jmlApiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
