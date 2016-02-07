3466d634c93b628a8bbedf7a701fb7895c9d9da7
fathergoose <al@ilseman.com>
2016-02-06 20:41:39 -0600

    wrote passing test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with a body and answer" do
+    @question = Question.new(
+      body: "testquestion",
+      answer: "testanswer")
+    expect(@question).to be_valid
+  end
+
 end
