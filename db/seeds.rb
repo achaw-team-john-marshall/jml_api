c7ea4e391e230852225e12937b93067723bae5b8
Dennise Saxton <dennisepnd@gmail.com>
2016-02-27 11:43:56 -0600

    added first two questionnaire questions to seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,10 @@ Question.create!([
   {body: "Am I pregnant", answer: "No"},
   {body: "Is this real life?", answer: "yes, david"}
 ])
+
+QuestionnaireChoice.create!([
+  {body: "Have you or your family members experienced domestic abuse on behalf of your spouse? Domestic abuse can range from physical abuse to emotional abuse. Domestic abuse also includes serious financial control, where you have no control over family finances and the filing of tax returns.",
+  position: 1},
+  {body: "Did you and your spouse file joint tax returns?",
+  position: 2}
+  ])
