287d7944a524f9d164cb1b99ef7d1cf18f87e8fe
fathergoose <al@ilseman.com>
2016-02-28 13:40:17 -0600

    generated questionnaireChoices route

---
+++ client/app/scripts/controllers/questionnairechoices.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jmlApiApp.controller:QuestionnairechoicesCtrl
+ * @description
+ * # QuestionnairechoicesCtrl
+ * Controller of the jmlApiApp
+ */
+angular.module('jmlApiApp')
+  .controller('QuestionnairechoicesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
